b975a09a37c916e46d8a8e61ac47f5b4e7ac19bd
Yuma Morimoto <kanten520212@gmail.com>
2024-08-09 02:06:07 +0900

    update(#17): コードや設定の追加修正

--- backend/functions/src/index.ts
+++ backend/functions/src/index.ts
@@ -1,25 +1,13 @@
-/**
- * Import function triggers from their respective submodules:
- *
- * import {onCall} from "firebase-functions/v2/https";
- * import {onDocumentWritten} from "firebase-functions/v2/firestore";
- *
- * See a full list of supported triggers at https://firebase.google.com/docs/functions
- */
 import express, { Request, Response } from "express";
-const { onRequest } = require('firebase-functions/v2/https');
-// import * as logger from "firebase-functions/logger";
-
+import { onRequest } from "firebase-functions/v2/https";
 const app = express();
 
-app.use((req: Request, res: Response) => {
-    res.header("Access-Control-Allow-Origin", "*");
-    res.header("Access-Control-Allow-Methods", "*")
-    res.header("Access-Control-Allow-Headers", "*");
-})
+app.get('/', (req: Request, res: Response) => {
+    res.send('request document root');
+});
 
-// build multiple CRUD interfaces:
-app.get('/', (req, res) => res.send(JSON.stringify({Key: 'helloWorld'})));
+app.get('/hello', (req: Request, res: Response) => {
+    res.send(JSON.stringify({Key: 'helloWorld'}));
+});
 
-// Expose Express API as a single Cloud Function:
-exports.widgets = onRequest(app);
+exports.api = onRequest(app);
